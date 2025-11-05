7737bef1ddccccbbba954a23ebdd89461dbcf6d4
Meshan Khosla <mkhosla@berkeley.edu>
2025-11-04 19:51:08 -0800

    Start of schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,7 +4,48 @@ import { v } from 'convex/values';
 export default defineSchema({
   numbers: defineTable({
     value: v.number(),
-    // Reference to Better Auth user table (Better Auth creates a "user" table)
     createdBy: v.string(),
   }).index('by_createdBy', ['createdBy']),
+
+  datasources: defineTable({
+    name: v.string(),
+    createdBy: v.string(),
+  }).index('by_createdBy', ['createdBy']),
+
+  analyses: defineTable({
+    datasourceIds: v.array(v.id('datasources')),
+    name: v.string(),
+    createdBy: v.string(),
+  }).index('by_createdBy', ['createdBy']),
+
+  sheets: defineTable({
+    name: v.string(),
+    analysisId: v.id('analyses'),
+    createdBy: v.string(),
+  })
+    .index('by_createdBy', ['createdBy'])
+    .index('by_analysisId', ['analysisId']),
+
+  dashboards: defineTable({
+    name: v.string(),
+    sourceAnalysisId: v.id('analyses'),
+    createdBy: v.string(),
+  }).index('by_createdBy', ['createdBy']),
+
+  visuals: defineTable({
+    type: v.union(
+      v.literal('table'),
+      v.literal('bar chart'),
+      v.literal('line chart'),
+      v.literal('pie chart'),
+    ),
+    title: v.string(),
+    position: v.object({
+      x: v.number(),
+      y: v.number(),
+      width: v.number(),
+      height: v.number(),
+    }),
+    createdBy: v.string(),
+  }),
 });
