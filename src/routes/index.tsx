b5d3bd759e01a51777201f7329f9787ad952170e
Meshan Khosla <mkhosla@berkeley.edu>
2025-11-04 18:28:32 -0800

    Example optimistic update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { convexQuery } from '@convex-dev/react-query';
 import { api } from 'convex/_generated/api';
 import { useMutation } from 'convex/react';
+import type { Id } from 'convex/_generated/dataModel';
 import { AppLayout } from '~/components/app-layout';
 import { Button } from '~/components/ui/button';
 
@@ -17,7 +18,27 @@ export const Route = createFileRoute('/')({
 
 function HomePage() {
   const { data: numbers } = useSuspenseQuery(convexQuery(api.numbers.list, {}));
-  const createNumber = useMutation(api.numbers.create);
+
+  const createNumber = useMutation(api.numbers.create).withOptimisticUpdate(
+    (localStore, args) => {
+      const { value } = args;
+      const existingNumbers = localStore.getQuery(api.numbers.list, {});
+
+      // If we've loaded the api.numbers.list query, add an optimistic number
+      if (existingNumbers !== undefined) {
+        const now = Date.now();
+        const newNumber = {
+          _id: crypto.randomUUID() as Id<'numbers'>,
+          _creationTime: now,
+          value,
+        };
+        localStore.setQuery(api.numbers.list, {}, [
+          ...existingNumbers,
+          newNumber,
+        ]);
+      }
+    },
+  );
 
   function createRandomNumber() {
     createNumber({ value: Math.floor(Math.random() * 10) + 1 });
