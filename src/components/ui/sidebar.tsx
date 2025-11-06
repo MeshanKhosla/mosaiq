0852fcb8bb7d1171cb632ca9738b5b518e385f72
Meshan Khosla <mkhosla@berkeley.edu>
2025-11-05 22:16:12 -0800

    Make sidebar faster

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -249,7 +249,7 @@ const Sidebar = forwardRef<
         {/* This is what handles the sidebar gap on desktop - pushes content */}
         <div
           className={cn(
-            'relative bg-transparent transition-[width] duration-200 ease-linear',
+            'relative bg-transparent transition-[width] duration-75 ease-linear',
             'group-data-[side=right]:rotate-180',
           )}
           style={{
@@ -265,7 +265,7 @@ const Sidebar = forwardRef<
         >
           <div
             className={cn(
-              'absolute inset-y-0 hidden h-svh transition-[width] duration-200 ease-linear md:flex',
+              'absolute inset-y-0 hidden h-svh transition-[width] duration-75 ease-linear md:flex',
               side === 'left' ? 'left-0' : 'right-0',
               // Adjust the padding for floating and inset variants.
               variant === 'floating' || variant === 'inset'
@@ -473,7 +473,7 @@ const SidebarGroupLabel = forwardRef<
       ref={ref}
       data-sidebar="group-label"
       className={cn(
-        'flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opacity] duration-200 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0',
+        'flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opacity] duration-75 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0',
         'group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0',
         className,
       )}
