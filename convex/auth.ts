882f30bee3c88314e6e3d74b9cc928c6daee7333
Meshan Khosla <mkhosla@berkeley.edu>
2025-11-14 23:59:49 -0800

    Auth url

--- convex/auth.ts
+++ convex/auth.ts
@@ -6,7 +6,7 @@ import { query } from './_generated/server';
 import type { DataModel } from './_generated/dataModel';
 import type { GenericCtx } from '@convex-dev/better-auth';
 
-const siteUrl = process.env.SITE_URL!;
+const siteUrl = process.env.SITE_URL_PROD ?? process.env.SITE_URL!;
 
 // The component client has methods needed for integrating Convex with Better Auth,
 // as well as helper methods for general use.
